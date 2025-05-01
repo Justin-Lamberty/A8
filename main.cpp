b64aadeb4ed6e106ebfeda0f13ea69b0fd6cd875
Justin-Lamberty <111925736+Justin-Lamberty@users.noreply.github.com>
2025-05-01 20:12:38 +0000

    idk

--- main.cpp
+++ main.cpp
@@ -6,11 +6,6 @@ int main() {
     Director* director = new Director();
     auto [luxuryCar, luxuryManual] = ClientCode4(*director);
 
-    std::cout << "Hello\n";
-    std::cout << luxuryCar->engine << "\n";
-    luxuryCar->listFeatures();
-    luxuryManual->listFeatures();
-
     delete luxuryCar;
     delete luxuryManual;
     delete director;
